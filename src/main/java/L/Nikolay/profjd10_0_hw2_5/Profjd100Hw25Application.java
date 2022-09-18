78c71bf25e2454311ae6a25c4f7c1ef2d89d1645
Николай Лукьянчиков <nicoolas.ru@gmail.com>
2022-09-18 18:02:15 +0300

    Первый коммит

---
+++ src/main/java/L/Nikolay/profjd10_0_hw2_5/Profjd100Hw25Application.java
@@ -0,0 +1,13 @@
+package L.Nikolay.profjd10_0_hw2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Profjd100Hw25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Profjd100Hw25Application.class, args);
+	}
+
+}
